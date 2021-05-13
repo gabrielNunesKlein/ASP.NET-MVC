f479bc28c069a97b5b6aa85ec1fa125dd660bee2
gabrielNunesKlein <gabrielgnk@outlook.com>
2021-05-13 15:32:37 -0300

    Realizando conexao  com banco e listando os dados

---
+++ Buiness/Pagina.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buiness
+{
+    public class Pagina
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Conteudo { get; set; }
+
+        public DateTime Data { get; set; }
+
+        public List<Pagina> Lista()
+        {
+            var lista = new List<Pagina>();
+            var paginasDb = new Database.Pagina();
+            foreach(DataRow row in paginasDb.Lista().Rows)
+            {
+                var pagina = new Pagina();
+                pagina.Id = Convert.ToInt32(row["id"]);
+                pagina.Nome = row["nome"].ToString();
+                pagina.Conteudo = row["conteudo"].ToString();
+                pagina.Data = Convert.ToDateTime(row["data"]);
+
+                lista.Add(pagina);
+            }
+
+            return lista;
+        }
+    }
+}
---
+++ Database/Pagina.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    public class Pagina
+    {
+        private string sqlConn()
+        {
+            return ConfigurationManager.AppSettings["sqlConn"];
+        }
+
+        public DataTable Lista()
+        {
+            using (SqlConnection connection = new SqlConnection(sqlConn()))
+            {
+                string queryString = "SELECT * FROM Paginas";
+
+                SqlCommand cmd = new SqlCommand(queryString, connection);
+                cmd.Connection.Open();
+
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                adapter.SelectCommand = cmd;
+
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                return dt;
+            }
+
+        }
+    }
+}
