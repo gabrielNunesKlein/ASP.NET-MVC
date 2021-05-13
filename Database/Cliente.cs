60c5ec213c72fd9c966ed854111b329995fc8cc6
gabrielNunesKlein <gabrielgnk@outlook.com>
2021-05-12 21:58:02 -0300

    Projeto criado

---
+++ Database/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+
+        public List<Cliente> clientes(string cliente)
+        {
+            var clientes = new List<Cliente>();
+            clientes.Add(new Cliente { Nome = "Gabriel" });
+            return clientes;
+        }
+    }
+}
